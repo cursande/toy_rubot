0f030395a69b3e312f12ad9327c258c5b8934427
Alex F <adfarries@gmail.com>
2021-05-22 16:20:02 +1000

    Move to fixture file for e2e test

--- spec/toy_rubot_spec.rb
+++ spec/toy_rubot_spec.rb
@@ -1,30 +1,8 @@
 RSpec.describe ToyRubot do
-  let(:path)  { 'test_instructions.txt' }
-
-  let(:test_file) do
-    Tempfile.open(path) do |f|
-      instructions.each { |instruction| f.write(instruction) }
-    end
-  end
-
-  let(:instructions) do
-    [
-      "ALICE: PLACE 1,2,EAST",
-      "ALICE: MOVE",
-      "ALICE: MOVE",
-      "ALICE: LEFT",
-      "BOB: PLACE 3,3,EAST",
-      "BOB: MOVE",
-      "ALICE: MOVE",
-      "ALICE: REPORT",
-      "BOB: RIGHT",
-      "BOB: MOVE",
-      "BOB: REPORT"
-    ]
-  end
+  let(:path)  { File.join(__dir__, 'fixtures', 'test_instructions.txt') }
 
   it 'outputs the correct positions of the robots when reporting' do
-    expect { ToyRobot::Runner.new(path).run }.to output(<<~OUT).to_stdout
+    expect { ToyRubot.run(path) }.to output(<<~OUT).to_stdout
           ALICE: 3,3,NORTH
           BOB: 4,2,SOUTH
       OUT
