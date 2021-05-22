7f5e3f775b50c01ead1eb9caf1896df3e51d7bf2
Alex F <adfarries@gmail.com>
2021-05-22 15:27:20 +1000

    Add main module spec

--- spec/toy_rubot_spec.rb
+++ spec/toy_rubot_spec.rb
@@ -1,9 +1,32 @@
 RSpec.describe ToyRubot do
-  it "has a version number" do
-    expect(ToyRubot::VERSION).not_to be nil
+  let(:path)  { 'test_instructions.txt' }
+
+  let(:test_file) do
+    Tempfile.open(path) do |f|
+      instructions.each { |instruction| f.write(instruction) }
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:instructions) do
+    [
+      "ALICE: PLACE 1,2,EAST",
+      "ALICE: MOVE",
+      "ALICE: MOVE",
+      "ALICE: LEFT",
+      "BOB: PLACE 3,3,EAST",
+      "BOB: MOVE",
+      "ALICE: MOVE",
+      "ALICE: REPORT",
+      "BOB: RIGHT",
+      "BOB: MOVE",
+      "BOB: REPORT"
+    ]
+  end
+
+  it 'outputs the correct positions of the robots when reporting' do
+    expect { ToyRobot::Runner.new(path).run }.to output(<<~OUT).to_stdout
+          ALICE: 3,3,NORTH
+          BOB: 4,2,SOUTH
+      OUT
   end
 end
